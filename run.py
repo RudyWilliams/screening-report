aebf03463b7130177fd63b48639c53bae3737bce
Rudy Williams <rudywilliams@crosswindsyouthservices.org>
2020-08-24 15:31:10 -0400

    adds sample output for boxplot

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from screening_report_tool.plotting.plots import create_screenings_boxplot
 filepath = "data/SampleScreeningData.xlsx"
 column_name = "Screenings"
 our_name = "K"
+output_name = "output/SampleScreeningBoxplot.html"
 
 data = read_data_from_excel(filepath=filepath)
 screenings = get_screenings(df=data, column_name=column_name)
@@ -15,3 +16,4 @@ our_number = get_our_screenings(df=data, column_name=column_name, our_name=our_n
 
 fig = create_screenings_boxplot(screenings_array=screenings, our_number=our_number)
 fig.show()
+fig.write_html(output_name)
