7a7441f54e17924f54ab40595e72b5603ec76661
Rudy Williams <rudywilliams@crosswindsyouthservices.org>
2020-08-27 15:38:02 -0400

    updates data transformations into class

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from screening_report_tool.preprocessing.read import read_data_from_excel
 from screening_report_tool.preprocessing.transform import (
     get_our_screenings,
     get_screenings,
+    Data,
 )
 from screening_report_tool.plotting.plots import create_screenings_boxplot
 
@@ -10,10 +11,13 @@ column_name = "Screenings"
 our_name = "K"
 output_name = "output/SampleScreeningBoxplot.html"
 
-data = read_data_from_excel(filepath=filepath)
-screenings = get_screenings(df=data, column_name=column_name)
-our_number = get_our_screenings(df=data, column_name=column_name, our_name=our_name)
+df = read_data_from_excel(filepath=filepath)
+data = Data(df=df, screening_column=column_name, our_name=our_name)
+data.set_screenings_array()
+data.set_us_data()
 
-fig = create_screenings_boxplot(screenings_array=screenings, our_number=our_number)
+fig = create_screenings_boxplot(
+    screenings_array=data.screenings_array, our_number=data.n_screenings_us
+)
 fig.show()
-fig.write_html(output_name)
+# fig.write_html(output_name)
--- screening_report_tool/preprocessing/transform.py
+++ screening_report_tool/preprocessing/transform.py
@@ -1,8 +1,58 @@
-def get_screenings(df, column_name):
-    # this assumes that the df has the form of the
-    # Total column being on the bottom of the df
-    return df[column_name][:-1]
+class Data:
+    def __init__(self, df, screening_column, our_name):
+        self.df = df
+        self.screening_column = screening_column
+        self.our_name = our_name
+        self.screenings_array = None
+        self.n_screenings_us = None
+        self.n_screenings_rest = None
+        self.us_array = None
+        self.rest_array = None
+        self.us_prop_array = None
+        self.rest_prop_array = None
+
+    def set_screenings_array(self):
+        self.screenings_array = self.df[self.screening_column].values[:-1]
+        return self
+
+    def set_us_data(self):
+        us_data = self.df.loc[self.our_name, :].values
+        us_array, n_screenings_us = us_data[:-1], us_data[-1]
+        self.us_array = us_array
+        self.n_screenings_us = n_screenings_us
+        self.us_prop_array = us_array / n_screenings_us
+        return self
+
+    def set_rest_data(self, total_row_name):
+        _df = self.df.copy()
+        not_us = ~(_df.index == self.our_name) & ~(_df.index == total_row_name)
+        rest_df = _df.loc[not_us, :]
+        # print(rest_df.sum(axis=0))
+        rest_agg_array_with_screenings = rest_df.sum(axis=0).values
+        rest_array, n_screenings_rest = (
+            rest_agg_array_with_screenings[:-1],
+            rest_agg_array_with_screenings[-1],
+        )
+        self.rest_array = rest_array
+        self.n_screenings_rest = n_screenings_rest
+        self.rest_prop_array = rest_array / n_screenings_rest
+        return self
 
 
-def get_our_screenings(df, column_name, our_name):
-    return df.loc[our_name, column_name]
+if __name__ == "__main__":
+
+    import pandas as pd
+
+    df = pd.read_excel("data/SampleScreeningData.xlsx", index_col=0)
+    data = Data(df=df, screening_column="Screenings", our_name="K")
+    data.set_screenings_array()
+    # print(data.screenings_array)
+    data.set_us_data()
+    print(data.us_array)
+    print(data.n_screenings_us)
+    print(data.us_prop_array)
+
+    data.set_rest_data(total_row_name="Total")
+    print(data.rest_array)
+    print(data.n_screenings_rest)
+    print(data.rest_prop_array)
