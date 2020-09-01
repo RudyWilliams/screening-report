afd89f3fc7f9d197ff906a54a3d565dd23a93e45
Rudy Williams <rudywilliams@crosswindsyouthservices.org>
2020-09-01 10:59:13 -0400

    adds type hints to read.py and path creator

--- screening_report_tool/preprocessing/read.py
+++ screening_report_tool/preprocessing/read.py
@@ -1,5 +1,16 @@
+from pathlib import Path
+from typing import Union
+
 import pandas as pd
 
 
-def read_data_from_excel(filepath):
+def create_filepath(
+    root: str, month: str, year: Union[str, int], filename: str
+) -> Path:
+    month = month.capitalize()
+    return Path(f"{root}/{month} {year}/{filename}")
+
+
+def read_data_from_excel(filepath: Path) -> pd.DataFrame:
     return pd.read_excel(filepath, index_col=0)
+
